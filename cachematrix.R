21c7e1fbc9d953aa8f69e9da1fe10a9cb35a5286
Thananya-S <138535425+Thananya-S@users.noreply.github.com>
2023-07-07 18:38:06 -0400

    Adding overall description of the function do and complete the makeCacheMatrix and cacheSolve function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## In this script, we will take an advantage of the scoping rules
+## of the R language by create a function that cache the inverse 
+## of a matrix rather than computing it repeatedly. 
+## This can help to reduce time-consuming computation by matrix conversion.
+##**************************************************************************
+## The first function is the makeCacheMatrix function. This function
+## creates special object that stores a matrix and caches its inverse.
+## Assuming that matrix is an invertible matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv.mat <- NULL
+  set <- function(y){
+    x <<- y
+    inv.mat <<- NULL
+  }
+  
+  get <- function(){ x }
+  setinverse <- function(){ inv.mat <<- solve(x) }
+  getinverse <- function(){ inv.mat }
+  
+  list(set = set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+##**************************************************************************
+## This second function is cacheSolve function. This function computes the 
+## inverse of the matrix returned by makeCacheMatrix above. If the inverse
+## has already been calculated (and the matrix has not changed), then
+## cacheSolve retrieve the inverse from the cache otherwise computes inverse
+## matrix using solve().
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv.mat <- x$getinverse()
+  if(!is.null(inv.mat)) {
+    message("getting cached data")
+    return(inv.mat)
+  } else{
+    mat <- x$get()
+    inv.mat <- solve(mat, ...)
+    return(inv.mat)
+  }
+  
 }
